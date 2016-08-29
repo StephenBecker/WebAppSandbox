b37e822c6c095692d2b46c063952bf3b9984ff66
Stephen Becker <sbecker@catalystitservices.com>
2016-08-29 15:14:17 -0400

    Added Katana console app

---
+++ KatanaConsoleApp/Controllers/GreetingsController.cs
@@ -0,0 +1,20 @@
+﻿using KatanaConsoleApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace KatanaConsoleApp.Controllers
+{
+    public class GreetingsController : ApiController
+    {
+        public Greeting Get(int? id = -1, string message = "")
+        {
+            string _message = message;
+            if (id != -1) { message = message + " Your Id =" + id; };
+            return new Greeting { Text = "Hello World! " + message };
+        }
+    }
+}
---
+++ KatanaConsoleApp/Program.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Owin.Hosting;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KatanaConsoleApp
+{
+    using System.IO;
+    using System.Web.Http;
+    using AppFunc = Func<IDictionary<string, object>, Task>;
+
+    public class HelloWorldComponent
+    {
+        private AppFunc _next;
+
+        public HelloWorldComponent(AppFunc next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(IDictionary<string, object> env)
+        {
+            var response = env["owin.ResponseBody"] as Stream;
+            using (var writer = new StreamWriter(response)) { return writer.WriteAsync("Hello!!"); }
+        }
+    }
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Use(async (environment, next) =>
+            {
+                Console.WriteLine("********************New Request*******************");
+                Console.WriteLine("Request : " + environment.Request.Uri);
+
+                await next();
+                Console.WriteLine("Response : " + environment.Response.StatusCode);
+            });
+
+            ConfigureWebApi(app);
+
+            app.Use<HelloWorldComponent>();
+        }
+
+        private void ConfigureWebApi(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(
+                "DefautlApi",
+                "api/{controller}/{id}",
+                new { id = RouteParameter.Optional }
+
+                );
+            app.UseWebApi(config);
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string uri = "http://localhost:8080";
+            using (WebApp.Start<Startup>(uri))
+            {
+                Console.WriteLine("Started");
+                Console.ReadKey();
+                Console.WriteLine("Stopping");
+            }
+        }
+    }
+}
